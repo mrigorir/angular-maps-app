249f21699da393a3248935ef55013d59fb893d4d
Marco Parra <marcoparra311@gmail.com>
2024-07-30 09:33:50 -0400

    Introduction to standalone components

--- src/app/alone/components/side-menu/side-menu.component.ts
+++ src/app/alone/components/side-menu/side-menu.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { MenuItem } from './interfaces/menu.interface';
-
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'maps-side-menu',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  selector: 'side-menu',
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
   menuItems: MenuItem[] = [
     {route: '/maps/fullScreen', name: 'Fullscreen'},
-    {route: '/maps/zoom-range', name: 'ZommRange'},
+    {route: '/maps/zoom-range', name: 'ZoomRange'},
     {route: '/maps/markers', name: 'Markers'},
     {route: '/maps/properties', name: 'Houses'},
+    {route: '/alone', name: 'Alone Page'},
   ]
 }
--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -3,18 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MinimapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkerPageComponent } from './pages/marker-page/marker-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+import { CounterAloneComponent } from "../alone/components/counter-alone/counter-alone.component";
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     MinimapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkerPageComponent,
@@ -23,7 +23,9 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
   ],
   imports: [
     CommonModule,
-    MapsRoutingModule
-  ]
+    MapsRoutingModule,
+    SideMenuComponent,
+    CounterAloneComponent
+]
 })
 export class MapsModule { }
